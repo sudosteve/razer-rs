ae430a86741ce5a6f29a162262ef437c052d8ecb
Stephen Thomas-Dorin <stephen.thomasdorin@gmail.com>
2023-07-22 01:51:06 -0700

    Create lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
+use std::i32;
+use std::vec::Vec;
+use udev::{Device, Enumerator};
+
+const RAZER_VENDOR_ID: i32 = 0x1532;
+
+pub struct RazerDevice {
+    device: Device,
+    name: String,
+}
+
+impl RazerDevice {
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn get_charge_level(&self) -> Option<i32> {
+        self.device.attribute_value("charge_level").map(|s| {
+            s.to_str().unwrap().parse::<i32>().unwrap() * 100 / 255
+        })
+    }
+
+    pub fn get_charge_status(&self) -> Option<bool> {
+        self.device.attribute_value("charge_status").map(|s| {
+            s.to_str().unwrap().eq("1")
+        })
+    }
+}
+
+pub fn get_devices() -> Vec<RazerDevice> {
+    let mut result: Vec<RazerDevice> = Vec::new();
+    let mut enumerator = Enumerator::new().unwrap();
+
+    enumerator.match_subsystem("hid").unwrap();
+
+    for device in enumerator.scan_devices().unwrap() {
+        let sysname = device.sysname().to_str().unwrap();
+        let (vendor_id, _) = get_vendor_id_device_id(sysname);
+
+        if vendor_id == RAZER_VENDOR_ID {
+            if let Some(device_type) = device.attribute_value("device_type") {
+                let name = String::from(device_type.to_str().unwrap());
+                result.push(RazerDevice{
+                    device: device,
+                    name: name,
+                });
+            }
+        }
+    }
+    result
+}
+
+fn get_vendor_id_device_id(sysname: &str) -> (i32, i32) {
+    let mut parts = sysname.split(":");
+    parts.next();
+    let vendor_id = i32::from_str_radix(parts.next().unwrap(), 16).unwrap();
+    let device_id_str = parts.next().unwrap().split(".").next().unwrap();
+    let device_id = i32::from_str_radix(device_id_str, 16).unwrap();
+    (vendor_id, device_id)
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,21 @@ use udev::Enumerator;
 
 fn main() {
     println!("Rusty Razer tool!");
-    list_devices();
+
+    let devices = razer_rs::get_devices();
+
+    for device in devices {
+        println!("{}: ", device.get_name());
+        if let Some(charge_level) = device.get_charge_level() {
+            println!("    charge: {}", charge_level);
+        }
+        if let Some(charge_status) = device.get_charge_status() {
+            println!("    charge: {}", charge_status);
+        }
+    }
 }
 
 // TODO add filters struct
 
 fn list_devices() {
-    let mut enumerator = Enumerator::new().unwrap();
-    let razer_vendor_id = 0x1532;
-
-    enumerator.match_subsystem("hid").unwrap();
-
-    for device in enumerator.scan_devices().unwrap() {
-        let sysname = device.sysname().to_str().unwrap();
-        let (vendor_id, _) = get_vendor_id_device_id(sysname);
-
-        if vendor_id == razer_vendor_id {
-            if let Some(device_type) = device.attribute_value("device_type") {
-                println!("{}: ", device_type.to_str().unwrap());
-                if let Some(charge_level_str) = device.attribute_value("charge_level") {
-                    let charge_level_str = charge_level_str.to_str().unwrap();
-                    let charge_level = charge_level_str.parse::<i32>().unwrap() * 100 / 255;
-                    println!("    charge: {}", charge_level);
-                }
-                if let Some(charge_status_str) = device.attribute_value("charge_status") {
-                    let charge_status_str = charge_status_str.to_str().unwrap();
-                    let charge_status = charge_status_str.eq("1");
-                    println!("    charging: {}", charge_status);
-                }
-            }
-        }
-    }
-}
-
-fn get_vendor_id_device_id(sysname: &str) -> (i32, i32) {
-    let mut parts = sysname.split(":");
-    parts.next();
-    let vendor_id = i32::from_str_radix(parts.next().unwrap(), 16).unwrap();
-    let device_id_str = parts.next().unwrap().split(".").next().unwrap();
-    let device_id = i32::from_str_radix(device_id_str, 16).unwrap();
-    (vendor_id, device_id)
 }
